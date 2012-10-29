fe3eacf89c47f48b82e75c8959ce60de4c673638
Ethan Sherbondy <ethanis@dhcp-18-111-5-234.dyn.mit.edu>
2012-10-28 23:39:03 -0400

    latex setup.

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 
 ;; marmalade
 ;; usage: M-x package-install
-;; nrepl, solarized-theme
+;; nrepl, solarized-theme, exec-path-from-shell
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -13,3 +13,33 @@
 
 ;; oh yeah
 (load-theme 'solarized-dark t)
+
+;; Enable eldoc in clojure buffers:
+(add-hook 'nrepl-interaction-mode-hook
+  'nrepl-turn-on-eldoc-mode)
+
+;; Stop the error buffer from popping up while working in the REPL buffer:
+(setq nrepl-popup-stacktraces nil)
+
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
+
+;; Setup environment variables from the user's shell.
+(when is-mac (exec-path-from-shell-initialize))
+
+;; make sure path is correct when launched as application
+(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(push "/usr/local/bin" exec-path)
+
+;; load tex packages
+(push "/usr/share/emacs/site-lisp" load-path)
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-PDF-mode t)
+(setq-default TeX-master nil)
+
+(setq TeX-output-view-style "open %o")
+
