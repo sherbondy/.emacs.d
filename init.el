29a8599868c876c5769cdf6e0b46a8ba3fc29c12
Ethan Sherbondy <sherbondye@gmail.com>
2013-02-11 03:31:13 -0500

    Autocomplete, setting up cedet.

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; must load cedet early on before eieio gets loaded
+(load-file "~/emacs/cedet-bzr/cedet-devel-load.el")
+
 (set-face-attribute 'default nil :font "mono-10")
 
 ;; hide the toolbar
@@ -14,10 +17,12 @@
 (defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
 (defvar melpa     '("melpa" . "http://melpa.milkbox.net/packages/"))
 (defvar gnu       '("gnu" . "http://elpa.gnu.org/packages"))
+(defvar org       '("org" . "http://orgmode.org/elpa/"))
 
 (add-to-list 'package-archives marmalade t)
 (add-to-list 'package-archives melpa t)
 (add-to-list 'package-archives gnu t)
+(add-to-list 'package-archives org t)
 
 (package-initialize)
 
@@ -33,18 +38,24 @@
 (defun init--install-packages ()
   (packages-install
    (cons 'exec-path-from-shell melpa)
+   (cons 'auto-complete melpa)
    (cons 'paredit melpa)
    (cons 'gist melpa)
    (cons 'clojure-mode melpa)
    (cons 'clojure-test-mode melpa)
    (cons 'nrepl melpa)
+   (cons 'ac-nrepl melpa)
    (cons 'solarized-theme melpa)
    (cons 'rainbow-delimiters melpa)
    (cons 'markdown-mode melpa)
    (cons 'haml-mode melpa)
    (cons 'scss-mode melpa)
+   (cons 'scala-mode2 melpa)
+   (cons 'auto-complete-clang melpa)
    (cons 'magit melpa)
-   (cons 'geiser marmalade)))
+   (cons 'cljsbuild-mode melpa)
+   (cons 'org org)
+   (cons 'undo-tree melpa)))
 
 (init--install-packages)
 
@@ -68,6 +79,7 @@
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (push "/usr/local/bin" exec-path)
 
+
 ;; load tex packages
 (push "/usr/share/emacs/site-lisp" load-path)
 (load "auctex.el" nil t t)
@@ -81,8 +93,6 @@
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 
 (setq TeX-source-correlate-method 'synctex)
-
-(setq inferior-lisp-program "browser-repl")
 
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\.cljx$" . clojure-mode))
@@ -101,22 +111,15 @@
 
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
-(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
-(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
-(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
-(add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-
-(setq scmutils-root "/usr/local/scmutils")
-
-(setq scheme-program-name
-      (concat scmutils-root
-      "/mit-scheme/bin/scheme "
-      "-library " scmutils-root "/mit-scheme/lib "
-      "-heap 6500"))
+(add-hook 'emacs-lisp-mode-hook       'paredit-mode)
+(add-hook 'lisp-mode-hook             'paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'paredit-mode)
+(add-hook 'scheme-mode-hook           'paredit-mode)
+(add-hook 'clojure-mode-hook          'paredit-mode)
+(add-hook 'nrepl-mode-hook            'paredit-mode)
 
 
+;; kibit setup
 ;; Teach compile the syntax of the kibit output
 (require 'compile)
 (add-to-list 'compilation-error-regexp-alist-alist
@@ -127,15 +130,65 @@
 ;; the current emacs buffer belongs to.
 (defun kibit ()
   "Run kibit on the current project.
-Display the results in a hyperlinked *compilation* buffer."
+   Display the results in a hyperlinked *compilation* buffer."
   (interactive)
   (compile "lein kibit"))
 
 
+;; markdown
 (autoload 'markdown-mode "markdown-mode.el" 
   "Major mode for editing Markdown files" t) 
 
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
-(setq quack-default-program "csi")
+
+;; scheme setup
 (require 'quack)
+(setq quack-default-program "csi")
+
+(setq scmutils-root "/usr/local/scmutils")
+(setq scheme-program-name
+      (concat scmutils-root
+      "/mit-scheme/bin/scheme "
+      "-library " scmutils-root "/mit-scheme/lib "
+      "-heap 6500"))
+
+;; jsim setup
+
+(autoload 'jsim-mode "jsim.el" nil t)
+(setq auto-mode-alist (cons '("\.jsim$" . jsim-mode) auto-mode-alist))
+(add-hook 'jsim-mode-hook 'turn-on-font-lock)
+
+
+;; autocomplete setup
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; cedet config
+
+(semantic-mode 1)
+(require 'semantic/ia)
+(require 'semantic/bovine/gcc)
+(require 'semantic/db-javap)
+
+;; to tell semantic where additional libs live, do:
+;; (semantic-add-system-include "/the/include/dir" c++-mode)
+
+;; imenu support for semantic tags
+(defun imenu-semantic-hook ()
+  (imenu-add-to-menubar "TAGS"))
+
+(add-hook 'semantic-init-hooks 'imenu-semantic-hook)
+
+;; ede projects mode from cedet
+(global-ede-mode t)
+
+;; use (C-c, TAB) for Senator name completion
+
+(defun my-c-mode-cedet-hook ()
+ (local-set-key "." 'semantic-complete-self-insert)
+ (local-set-key ">" 'semantic-complete-self-insert))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-cedet-hook)
+;; Evaluation of this code will lead to execution of the semantic-complete-self-insert command when user will press . or > after variables, that are instances of some data structure, and displaying a list of possible completions for given class or structure.
